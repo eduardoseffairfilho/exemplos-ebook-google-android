80ffe02c1c0f1b8892364853f13ebff5e8135adf
eduardoseffairfilho@gmail.com <eduardoseffairfilho@gmail.com@6b2fe269-770d-e008-7c09-9c39740f9640>
2014-07-22 19:45:23 +0000

    Cria a estrutura do diretório que irá salvar a imagem capturada?

--- trunk/ExemploMediaStore/src/com/example/exemplomediastore/MainActivity.java
+++ trunk/ExemploMediaStore/src/com/example/exemplomediastore/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.exemplomediastore;
 
-import android.support.v7.app.ActionBarActivity;
+import java.io.File;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,7 +16,35 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
+		
+		File dir1 = new File("/sdcard");
+		File dir2 = null;
+		Log.d("Teste",  "Dir 1 existe?"+dir1.exists());
+		if (dir1.exists()) {
+			dir2 = new File(dir1,"LivroDeAndroid");
+		}
+		
+		Log.d("Teste",  "Dir 1 possui permissão de escrita?"+dir1.canWrite());
+		
+		if (dir2 != null) {
+			Log.d("Teste",  "Dir 2 existe?"+dir2.exists());
+			if (dir2.exists() == false) {
+				boolean result = dir2.mkdirs();
+				Log.d("Teste",  "Dir2 foi criado?"+result);
+			}
+			Log.d("Teste",  "Dir 2 já existe?"+dir2.exists());
+			
+			if (dir2.exists()) {
+				File file = new File(dir2, "hello_camera.jpg");
+				Log.d("Teste",  "Arquivo já existe?"+file.exists());
+				Uri uri = Uri.fromFile(file);
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+				startActivity(intent);
+				Log.d("Teste",  "Arquivo foi criado?"+file.exists());
+			}
+		}
 	}
 
 	@Override
