1e0197e40b24669a50879b98bc1314c5ccf7da10
eduardoseffairfilho@gmail.com <eduardoseffairfilho@gmail.com@6b2fe269-770d-e008-7c09-9c39740f9640>
2014-07-23 21:10:01 +0000

    corrigindo bugs.

--- trunk/Ebook_ExemploAbreContratos/src/com/example/exemploabrecontratos/MainActivity.java
+++ trunk/Ebook_ExemploAbreContratos/src/com/example/exemploabrecontratos/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.exemploabrecontratos;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
