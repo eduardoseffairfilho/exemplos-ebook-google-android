69e1b7aca1775f79be60071a84cf64754b240fcc
eduardoseffairfilho@gmail.com <eduardoseffairfilho@gmail.com@6b2fe269-770d-e008-7c09-9c39740f9640>
2014-07-23 21:22:55 +0000

    corrigindo bugs.

--- Ebook_ExemploAbreNavegador/src/com/example/exemploabrenavegador/MainActivity.java
+++ Ebook_ExemploAbreNavegador/src/com/example/exemploabrenavegador/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.exemploabrenavegador;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
