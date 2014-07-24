ed5417fd36cff31663e3e44a99fe54caa9da3185
eduardoseffairfilho@gmail.com <eduardoseffairfilho@gmail.com@6b2fe269-770d-e008-7c09-9c39740f9640>
2014-07-24 12:16:18 +0000

    corrigindo bugs.

--- Ebook_ExemploMediaStore/src/com/example/exemplomediastore/MainActivity.java
+++ Ebook_ExemploMediaStore/src/com/example/exemplomediastore/MainActivity.java
@@ -2,16 +2,14 @@ package com.example.exemplomediastore;
 
 import java.io.File;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,24 +43,5 @@ public class MainActivity extends ActionBarActivity {
 				Log.d("Teste",  "Arquivo foi criado?"+file.exists());
 			}
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
